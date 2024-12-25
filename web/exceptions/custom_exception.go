db085b37d9b17b9ed789aad31f5fe97023bd471d
allen <allendeng2018@gmail.com>
2024-12-25 15:05:42 +0800

    增加异常try-Catch

--- web/exceptions/custom_exception.go
+++ web/exceptions/custom_exception.go
@@ -87,3 +87,35 @@ func (e *CustomError) GetI18nMsgTemplate() string {
 	}
 	return fmt.Sprintf("${err.%d:%s}", e.Code, e.Message)
 }
+
+func TryCatch(try func() (interface{}, error), catch func(error), finally func()) interface{} {
+	// 默认 catch 和 finally 为无操作函数
+	if catch == nil {
+		catch = func(err error) {}
+	}
+	if finally == nil {
+		finally = func() {}
+	}
+
+	// 使用 defer 确保 finally 被执行
+	defer finally()
+
+	// 使用 defer 捕获 panic
+	defer func() {
+		if r := recover(); r != nil {
+			// 如果发生 panic，调用 catch 来处理 panic
+			catch(fmt.Errorf("panic caught: %v", r))
+		}
+	}()
+
+	// 执行 try 代码块
+	data, err := try()
+	if err != nil {
+		// 如果有错误，调用 catch 块处理错误
+		catch(err)
+		return nil
+	}
+
+	// 没有错误时，返回结果
+	return data
+}
